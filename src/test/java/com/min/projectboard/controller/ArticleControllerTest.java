81d3fa8ab4e3c5ea39c098f687cce8d7e6ead7ec
Kimminhyeo <ualsgur99@gmail.com>
2023-03-01 15:09:13 +0900

    게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서 부가적인 옵션이 
    들어갈 수 있음을 확인했기 때문에 
    적절히 테스트 메소드 수정

--- src/test/java/com/min/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/min/projectboard/controller/ArticleControllerTest.java
@@ -23,7 +23,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
+    //@Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -32,7 +32,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
@@ -64,7 +64,7 @@ class ArticleControllerTest {
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
-    
+
     @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
